1c9e926de6cc8d8767aa8490865c84d14cd71e5c
Fabiano S Ramos <fabiano.ramos@somagrupo.com.br>
2022-11-12 22:50:11 -0300

    chore: add tictactoe class

---
+++ tic_tac_toe.py
@@ -0,0 +1,35 @@
+import random
+import os
+
+
+class ticTacToe:
+    def __init__(self):
+        self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+        self.done = ""
+
+    def print_board(self):
+        print("")
+        print(" " + self.board[0][0] + " | " +
+              self.board[0][1] + " | " + self.board[0][2])
+        print("---------------")
+        print(" " + self.board[1][0] + " | " +
+              self.board[1][1] + " | " + self.board[1][2])
+        print("---------------")
+        print(" " + self.board[2][0] + " | " +
+              self.board[2][1] + " | " + self.board[2][2])
+
+    def reset(self):
+        pass
+
+    def check_win_or_draw(self):
+        pass
+
+    def get_player_move(self):
+        pass
+
+    def make_move(self):
+        pass
+
+
+self = ticTacToe()
+self.print_board()
